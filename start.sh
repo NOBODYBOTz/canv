03ec17a7637b51e78be2ed2114b07c139a812c51
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-19 16:14:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
-docker build -t TG-Livegram-Bot .
-docker run -it TG-Livegram-Bot
+#!/bin/sh
+
 # Any initial setup or commands
 # For example, echo a message or run setup scripts
 
+# Start the Python application
 exec python -m Assistant.__main__
