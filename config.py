9f946ad7e721056df9ab36be859e4d0f45d04426
NOBODY BOTz <140589004+NOBODYBOTz@users.noreply.github.com>
2024-11-05 12:31:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ load_dotenv()
 
 TOKEN = getenv("TOKEN", None)
 "Telegram bot token obtained from botfather"
-DB_URI = getenv("MONGO_URL", None)
+DB_URI = getenv("MONGO_URL")
 "database url (mongo)"
-OWNER_ID = int(getenv("OWNER_ID", 1715861188))
+OWNER_ID = int(getenv("OWNER_ID", None))
 "Telegram ID of the bot owner"
-LOGGER_ID = int(getenv("LOGGER_ID", -1001955617973))
+LOGGER_ID = int(getenv("LOGGER_ID", None))
 "channel/group ID with `-` for keeping track of new errors where the bot gets..."
-SUDO_USER = list(map(int, getenv("SUDO_USER", "1715861188").split()))
+SUDO_USER = list(map(int, getenv("SUDO_USER", "").split()))
 "set of user ID which can have elevated privileges"
 LOGGER_LEVEL = getenv("LOGGER_LEVEL", 20)
 "logger level, `debug(10)`, `info(20)`, `warn(30)` and `error(40)`. default is `info`"
